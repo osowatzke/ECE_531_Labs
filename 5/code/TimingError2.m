4991269818a19639d1767039ed7ea5da6e610846
Owen <osowatzke@gmail.com>
2025-04-03 01:36:22 -0700

    Starting working on part 5

---
+++ 5/code/TimingError2.m
@@ -0,0 +1,69 @@
+%% General system details
+numSamples = 10000;
+modulationOrder = 2;
+snr = 5:5:40;
+delay = 0.25;
+N = 8;
+NF = 4;
+clear mod;
+evm_dB = zeros(size(snr));
+cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Baseband');
+cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Baseband with Timing Offset');
+cdPre.Position(1) = 50;
+cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
+%% Loop for each SNR
+for i = 1:length(snr)
+    %% Generate symbols
+    rng(0);
+    data = randi([0 modulationOrder-1], numSamples*2, 1);
+    mod = comm.DBPSKModulator(); modulatedData = real(mod(data));
+    %% Add TX/RX Filters
+    TxFlt = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol',N);
+    RxFlt = comm.RaisedCosineReceiveFilter('DecimationFactor',NF);
+    RxFltRef = clone(RxFlt);
+    %% Add noise source
+    chan = comm.AWGNChannel('NoiseMethod','Signal to noise ratio (SNR)','SNR',snr(i), ...
+        'SignalPower',1,'RandomStream', 'mt19937ar with seed');
+    %% Add delay
+    varDelay = dsp.VariableFractionalDelay;
+    filteredTXData = TxFlt(modulatedData);
+    noisyData = chan(filteredTXData);
+    offsetData = varDelay(noisyData, delay*N);
+    filteredRXData = RxFlt(offsetData);
+    filteredRxDataRef = RxFltRef(filteredTXData);
+    %% Test Each Symbol Sychronizer
+    % MATLAB built-in
+    symbolSync = comm.SymbolSynchronizer(...
+        SamplesPerSymbol=2,...
+        NormalizedLoopBandwidth=0.01, ...
+        DetectorGain=5.4,...
+        DampingFactor=1.0,...
+        TimingErrorDetector="Zero-Crossing (decision-directed)");
+    [rxSync, timingErr] = symbolSync(filteredRXData);
+    rxRef = filteredRxDataRef(1:2:end);
+    % Align data
+    ccOut = xcorr(rxRef,rxSync,32);
+    [~, maxIdx] = max(abs(ccOut));
+    dly = 33 - maxIdx;
+    rxSync = rxSync((dly+1):end);
+    % Make sure data is the same length
+    minSize = min(length(rxSync),length(rxRef));
+    rxSync = rxSync(1:minSize);
+    rxRef = rxRef(1:minSize);
+    % Compute EVM
+    evm = comm.EVM();
+    e = evm(rxRef(1000:end), rxSync(1000:end));
+    evm_dB(i) = 20*log10(e/100);
+    % Plots
+    cdPre(rxRef(1000:end));
+    cdPost(rxSync(1000:end));
+end
+
+figure(1);
+clf;
+plot(snr,evm_dB,'LineWidth',1.5);
+grid on;
+xlabel('SNR (dB)');
+ylabel('EVM (dB)')
