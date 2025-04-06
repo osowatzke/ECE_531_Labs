6733ce56de3c8b87b49da2605dcef695215a238d
Owen <osowatzke@gmail.com>
2025-04-06 04:08:52 -0700

    Saving script updates

--- 5/code/simTimingSync.m
+++ 5/code/simTimingSync.m
@@ -6,8 +6,8 @@ addpath('./timingCorrection')
 %% Generate System Details
 % Section 4: numFrames=200, frameSize=1024
 % Section 5: numFrames=1, frameSize=10000
-numFrames = 1;
-frameSize = 10000;
+numFrames = 200;
+frameSize = 1024;
 numSamples = numFrames*frameSize;
 samplesPerSymbol = 8;
 filterSymbolSpan = 10;
@@ -15,12 +15,12 @@ filterSymbolSpan = 10;
 snr_dB = 5:5:40;
 delay = 0;
 delayDrift = 0.01;
-phaseOffset = pi/4;
+phaseOffset = 0;
 %% Simulation Flags
 showConstellations = false;
 debugControlSystem = false;
 useIdealRef = true;
-useDspkMod = true;
+useDspkMod = false;
 useBuiltInSync = true;
 %% Select Symbol Synchronizer
 if useBuiltInSync
