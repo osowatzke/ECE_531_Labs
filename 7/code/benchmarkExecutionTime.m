adf4443ac5095172c58b17d7c63b0d7d26082f6f
Owen <osowatzke@gmail.com>
2025-04-13 01:03:32 -0700

    Updating benchmarking script

--- 7/code/benchmarkExecutionTime.m
+++ 7/code/benchmarkExecutionTime.m
@@ -1,13 +1,17 @@
 % Configure preamble position
 gapLen = 100;
 gapLenEnd = 200;
-gen = @(Len) 2*randi([0 1],Len,1)-1;
+
+% Note we create low-power noise instead of BPSK symbols so we have 0
+% Probability of error. This is useful for ensuring we can correctly
+% determine the start of the frame, but will not happen in practice.
+gen = @(Len) 0.01*randn(Len,1);
 
 % Number of runs to do at each sequence length
-numRuns = 2048;
+numRuns = 1000;
 
 % Sequence lengths to test
-sequenceLength = 10:50:2000;
+sequenceLength = 10:10:2000;
 
 % Allocate arrays for execution time
 xcorrExecutionTime = zeros(numRuns, length(sequenceLength));
@@ -27,11 +31,11 @@ for j = 1:length(sequenceLength)
     % Generate matched filter for filter implementation
     h = flip(conj(seq));
 
-    % Create received data
-    y = [gen(gapLen); seq; gen(gapLenEnd)];
-
     % Loop for each monte carlo run
     for i = 1:numRuns
+
+        % Create received data
+        y = [gen(gapLen); seq; gen(gapLenEnd)];
 
         % Capture xcorr output and execution time
         tic; corr = xcorr(y,seq);
@@ -57,7 +61,7 @@ for j = 1:length(sequenceLength)
     end
 end
 
-% Compute probability of error
+% Compute error probabilities
 xcorrProbErrors = xcorrNumErrors/numRuns;
 filterNumErrors = filterNumErrors/numRuns;
 
@@ -65,15 +69,33 @@ filterNumErrors = filterNumErrors/numRuns;
 fprintf('Max xcorr Error Probability: %.4f\n', max(xcorrProbErrors));
 fprintf('Max filter Error Probability: %.4f\n', max(filterNumErrors));
 
+% Get average execution times
+xcorrAvgExecutionTime = mean(xcorrExecutionTime);
+filterAvgExecutionTime = mean(filterExecutionTime);
+
+% Compute speedup
+speedup = xcorrAvgExecutionTime./filterAvgExecutionTime;
+
 % Plot exeuction times
 figure(1);
 clf;
 hold on;
-plot(sequenceLength, mean(xcorrExecutionTime));
-plot(sequenceLength, mean(filterExecutionTime));
+plot(sequenceLength, xcorrAvgExecutionTime);
+plot(sequenceLength, filterAvgExecutionTime);
 box on;
 grid on;
 title('Execution Time Comparison')
 legend('xcorr','filter');
 xlabel('Sequence Length');
 ylabel('Execution Time (s)')
+
+% Plot speedup
+figure(2);
+clf;
+hold on;
+plot(sequenceLength, speedup);
+box on;
+grid on;
+title('Speedup from ''filter'' Command')
+xlabel('Sequence Length');
+ylabel('Speedup')
