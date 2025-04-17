f670cee19944d1cd88cd1e6baec780bda24c4a46
Owen <osowatzke@gmail.com>
2025-04-16 21:01:38 -0700

    Normalizing input signal

--- 7/code/PreambleDetector.m
+++ 7/code/PreambleDetector.m
@@ -3,11 +3,13 @@ classdef PreambleDetector < matlab.System
         Threshold = 3;
         Preamble = [1+1i; 1-1i];
         Detections = 'All';
+        Normalize = false;
     end
     properties(Access=protected)
         detectAll;
         dataR;
         mf;
+        mfPwr;
     end
     methods
         % Initialize class properties with name-value pairs
@@ -25,6 +27,9 @@ classdef PreambleDetector < matlab.System
             % Determine matched filter
             self.mf = flip(conj(self.Preamble));
 
+            % Determine power of matched filter
+            self.mfPwr = sum(abs(self.mf).^2);
+
             % Determine detection mode
             self.detectAll = strcmp(self.Detections,'All');
         end
@@ -37,11 +42,26 @@ classdef PreambleDetector < matlab.System
             % Concatenate with previously input data
             data = [self.dataR; data(:)];
 
+            % Get power of input signal
+            dataPwr = data.*conj(data);
+
+            % Get average power of input signal
+            h = ones(size(self.mf));
+            dataPwr = filter(h,1,dataPwr);
+
+            % Remove appended data samples
+            dataPwr = dataPwr(length(self.Preamble):end);
+
             % Run matched filter on input
             crossCorr = filter(self.mf,1,data);
 
             % Remove appended data samples
             crossCorr = crossCorr(length(self.Preamble):end);
+
+            % Normalize cross correlation
+            if self.Normalize
+                crossCorr = crossCorr./(sqrt(dataPwr)*sqrt(self.mfPwr));
+            end
             
             % Detect end of preamble
             if self.detectAll
--- 7/code/rocCurve.m
+++ 7/code/rocCurve.m
@@ -1,20 +1,23 @@
 bits = ASCII2bits('Arizona');
-SNR_dB = -10:2:10;
-treshold = 0:1:40;
-numRuns = 100;
+SNR_dB = -10:5:10;
+treshold = 0:0.1:0.9;
+numRuns = 1000;
 codeGen = comm.BarkerCode('Length',7,'SamplesPerFrame',13);
 preamble = codeGen();
 dataLen = 500;
 symbolGen = @(len) pskmod(randi([0 1], len, 1), 2);
+figure(1); clf;
 for i = 1:length(SNR_dB)
     falseAlarms = zeros(1,length(treshold));
     missedDetections = zeros(1,length(treshold));
     probFalseAlarm = zeros(1,length(treshold));
     probMissedDetection = zeros(1,length(treshold));
     for j = 1:length(treshold)
-        prbdet = comm.PreambleDetector(...
+        prbdet = PreambleDetector(...
             'Preamble',  preamble,...
+            'Normalize', true,...
             'Threshold', treshold(j));
+        displayThresh = true;
         for k = 1:numRuns
             delay = randi([0 dataLen-length(preamble)]);
             numSymbolsStart = delay;
@@ -22,6 +25,10 @@ for i = 1:length(SNR_dB)
             data = [symbolGen(numSymbolsStart); preamble; symbolGen(numSymbolsEnd)];
             data = awgn(data,SNR_dB(i));
             idx = prbdet(data);
+            if isempty(idx) && displayThresh
+                disp(treshold(j))
+                displayThresh = false;
+            end
             delayEst = idx - length(preamble);
             missedDetections(j) = missedDetections(j) + ~any(delayEst == delay);
             falseAlarms(j) = falseAlarms(j) + sum(delayEst ~= delay);
@@ -34,6 +41,11 @@ for i = 1:length(SNR_dB)
         probMissedDetection(j) = missedDetections(j)/(numRuns);
     end
     probDetection = 1 - probMissedDetection;
-    plot(probFalseAlarm, probDetection);
+    plot(probFalseAlarm, probDetection,'LineWidth',1.5);
     hold on;
 end
+legend(cellfun(@(x)sprintf('SNR(dB)=%d',x),num2cell(SNR_dB),...
+    'UniformOutput',false),'location','best');
+grid on;
+xlabel('Probability of False Alarm');
+ylabel('Probability of Detection')
