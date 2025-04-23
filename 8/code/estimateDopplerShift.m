286cbb2bfaac9b92dc637ba416f9dfc8d2c3e6f1
Owen <osowatzke@gmail.com>
2025-04-23 01:00:11 -0700

    Adding routine for point target

---
+++ 8/code/estimateDopplerShift.m
@@ -0,0 +1,53 @@
+% Radius in meters
+r = 0.25;
+
+% Distance to fan 
+d = 0.4;
+
+% Get wavelength
+fc = 2.4e9;
+c = 3e8;
+lambda = c/fc;
+
+% Define rotation in rpm
+w = 1200;
+
+% Convert to rad/s
+w = w*(2*pi)/60;
+
+% Radial distance as a function of time
+dist = @(t) sqrt(r^2*cos(w*t).^2 + (r*sin(w*t) + r).^2 + d^2);
+
+% Create time vector over one cycle of movement
+dt = 1e-5;
+t = 0:dt:(2*pi/w);
+
+% Compute the doppler shifts as a function of time
+fd = zeros(1,length(t));
+for i = 1:length(t)
+    dr = (dist(t(i)+dt) - dist(t(i)))/dt;
+    fd(i) = 2*dr/lambda;
+end
+
+% Plot the doppler shift against time
+figure(1); clf;
+plot(t*1e3, fd);
+grid on;
+xlabel('Time (ms)')
+ylabel('Doppler shift (Hz)')
+
+% Compute a frequency axis for plotting
+fs = 1/dt;
+faxis = fs*(0:(length(t)-1))/(length(t));
+faxis = faxis - fs*(faxis >= fs/2);
+faxis = fftshift(faxis);
+
+% Determine the phase of the resulting waveform
+phi = filter(1,[1 -1],2*pi*fd.*dt);
+
+% Plot the spectrum of the return
+figure(2); clf;
+plot(faxis/1e3,db(fftshift(fft(exp(1i*phi)))))
+grid on;
+xlabel('Frequency (kHz)')
+ylabel('Magnitude (dB)')
